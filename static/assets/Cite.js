73d1a576b99c29c0f750eccc0b639c03a7d308f1
OmnisyR <68898477+OmnisyR@users.noreply.github.com>
2025-07-23 21:37:47 +0800

    Update Cite.js

--- static/assets/Cite.js
+++ static/assets/Cite.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
   var contentContainer = document.getElementById('content');
   const headings = contentContainer.querySelectorAll('\cite{');
   headings.forEach(heading => {
-        headings = ?
+        headings = '?'
     });
 });
