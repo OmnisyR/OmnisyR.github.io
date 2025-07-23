b75f85f9b31360623ca0d672c0b9b1b88a2cc02c
OmnisyR <lxuan233@gmail.com>
2025-07-23 13:38:29 +0000

    🎉auto update by Gmeek action

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 ### :page_facing_up: [2](https://OmnisyR.github.io/tag.html) 
 ### :speech_balloon: 0 
 ### :hibiscus: 587 
-### :alarm_clock: 2025-07-23 21:33:43 
+### :alarm_clock: 2025-07-23 21:38:29 
 ### Powered by :heart: [Gmeek](https://github.com/Meekdai/Gmeek)
---
+++ docs/assets/Cite.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var contentContainer = document.getElementById('content');
+  const headings = contentContainer.querySelectorAll('\cite{');
+  headings.forEach(heading => {
+        headings = '?'
+    });
+});
