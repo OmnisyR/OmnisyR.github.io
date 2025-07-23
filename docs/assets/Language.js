c9f2102ef92db7d041f25f945894d6e8bd1128e3
OmnisyR <lxuan233@gmail.com>
2025-07-23 15:05:45 +0000

    🎉auto update by Gmeek action

--- docs/assets/Language.js
+++ docs/assets/Language.js
@@ -1,15 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-  ["listTitle", "header", "content"].forEach((item, i) => {
-    var container = document.getElementById(item);
-    var content = container.innerHTML
-    var lan = 'e'
-    if(document.documentElement.lang === "zh-CN"){
-      var pa1 = /\\c(.*?)\\c/g
-      var pa2 = /\\e(.*?)\\e/g
-    }else{
-      var pa1 = /\\e(.*?)\\e/g
-      var pa2 = /\\c(.*?)\\c/g
-    }
-    container.innerHTML = content.replace(pa1, (match, content)=>{return content}).replace(pa2,'')
-  });
+function translate(container) {
+  var content = container.innerHTML
+  var lan = 'e'
+  if(document.documentElement.lang === 'zh-CN'){
+    var pa1 = /\\c(.*?)\\c/g
+    var pa2 = /\\e(.*?)\\e/g
+  }else{
+    var pa1 = /\\e(.*?)\\e/g
+    var pa2 = /\\c(.*?)\\c/g
+  }
+  container.innerHTML = content.replace(pa1, (match, content)=>{return content}).replace(pa2,'')
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  translate(document.getElementById('listTitle'))
+  translate(document.getElementById('content'))
 });
