37257ced0251c0f76878c75b362a2c34fc7fd2f2
OmnisyR <68898477+OmnisyR@users.noreply.github.com>
2025-07-23 22:55:08 +0800

    Update Language.js

--- static/assets/Language.js
+++ static/assets/Language.js
@@ -1,13 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var contentContainer = document.getElementById('content');
-  var content = contentContainer.innerHTML
-  var lan = 'e'
-  if (document.documentElement.lang === "zh-CN"){
-    var pa1 = /\\c(.*?)\\c/g
-    var pa2 = /\\e(.*?)\\e/g
-  }else{
-    var pa1 = /\\e(.*?)\\e/g
-    var pa2 = /\\c(.*?)\\c/g
-  }
-  contentContainer.innerHTML = content.replace(pa1, (match, content)=>{return content}).replace(pa2,'')
+  ["listTitle", "postTitle", "content"].forEach((item, i) => {
+    var container = document.getElementById(item);
+    var content = container.innerHTML
+    var lan = 'e'
+    if(document.documentElement.lang === "zh-CN"){
+      var pa1 = /\\c(.*?)\\c/g
+      var pa2 = /\\e(.*?)\\e/g
+    }else{
+      var pa1 = /\\e(.*?)\\e/g
+      var pa2 = /\\c(.*?)\\c/g
+    }
+    container.innerHTML = content.replace(pa1, (match, content)=>{return content}).replace(pa2,'')
+  });
 });
