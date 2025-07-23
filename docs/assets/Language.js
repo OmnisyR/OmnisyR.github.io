484a5e6d5ef69da9e5957852861c85d40ba3aaa1
OmnisyR <lxuan233@gmail.com>
2025-07-23 15:40:01 +0000

    🎉auto update by Gmeek action

--- docs/assets/Language.js
+++ docs/assets/Language.js
@@ -18,6 +18,6 @@ document.addEventListener('DOMContentLoaded', function() {
   translate(document.getElementById('content'))
   let elements = document.getElementsByClassName('postTitle')
   if (elements.length > 0) {
-    translate(elements[0]))
+    translate(elements[0])
   }
 });
