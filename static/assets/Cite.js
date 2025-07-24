14e3436b43e4da73e3f8f83ed7aff66d3f4f8df4
OmnisyR <68898477+OmnisyR@users.noreply.github.com>
2025-07-24 14:29:40 +0800

    Delete static/assets/Cite.js

--- static/assets/Cite.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var contentContainer = document.getElementById('content');
-  const headings = contentContainer.querySelectorAll('\cite{');
-  headings.forEach(heading => {
-        headings = '?'
-    });
-});
