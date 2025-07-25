a65d48e28c49d2e1370d3f2eb00ee9e2f96d5b83
OmnisyR <lxuan233@gmail.com>
2025-07-25 08:02:57 +0000

    🎉auto update by Gmeek action

--- docs/assets/HyperTOC.js
+++ docs/assets/HyperTOC.js
@@ -3,7 +3,7 @@ const denotes = new Map();
 document.addEventListener('DOMContentLoaded', function() {
   var container = document.getElementById('content')
   container.innerHTML = container.innerHTML.replace(/\\denotes([\s\S]*?)\\denotes/gm, (match, content) => {
-    content.replace(/{(.*?)}/g, (m, c) => {
+    content.replace(/😭(.*?)😭/g, (m, c) => {
       let arr = c.split('::')
       denotes.set(arr[0], arr[1])
       return ''
