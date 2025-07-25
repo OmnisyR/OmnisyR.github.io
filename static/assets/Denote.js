95b4cdc6163cc209fa65d25f72929318ced458f7
OmnisyR <68898477+OmnisyR@users.noreply.github.com>
2025-07-25 09:05:24 +0800

    Create Denote.js

---
+++ static/assets/Denote.js
@@ -0,0 +1,36 @@
+function createDenote() {
+  var container = document.getElementById('content')
+  container.innerHTML = container.innerHTML.replace(/\\denote{(.*?)}/g, (match, content) => {
+    console.log(content)
+  })
+  var denoteElement = document.createElement('div');
+  denoteElement.className = 'denote';
+}
+
+window.addEventListener('load', function() {
+  createDenote();
+  var css = `
+  .denote{
+    position: fixed;
+    top: 130px;
+    left: 50%;
+    transform: translateX(-100%) translateX(-420px);
+    width: 200px;
+    border: 1px solid #e1e4e8;
+    border-radius: 6px;
+    padding: 10px;
+    overflow-y: auto;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+    max-height: 70vh;
+  }
+  .denote-title{
+
+  }
+  .denote-content{
+
+  }
+  `
+  // const style = document.createElement('style');
+  // style.textContent = css;
+  // document.head.appendChild(style);
+});
