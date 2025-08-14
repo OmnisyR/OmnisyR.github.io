56cb95be766312776cd8c7b3a597802711796fdd
OmnisyR <lxuan233@gmail.com>
2025-08-14 02:03:13 +0000

    🎉auto update by Gmeek action

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 ### :page_facing_up: [3](https://OmnisyR.github.io/tag.html) 
 ### :speech_balloon: 0 
 ### :hibiscus: 30835 
-### :alarm_clock: 2025-08-14 09:51:14 
+### :alarm_clock: 2025-08-14 10:03:13 
 ### Powered by :heart: [Gmeek](https://github.com/Meekdai/Gmeek)
--- docs/assets/HyperTOC.js
+++ docs/assets/HyperTOC.js
@@ -1,8 +1,20 @@
 const denotes = new Map();
 
-document.addEventListener('DOMContentLoaded', function() {
+// document.addEventListener('DOMContentLoaded', function() {
+//   var container = document.getElementById('content')
+//   container.innerHTML = container.innerHTML.replace(/\\denotes([\s\S]*?)\\denotes/gm, (match, content) => {
+//     content.replace(/\;;;;([\s\S]*?);;;;/g, (m, c) => {
+//       let arr = c.split('::')
+//       denotes.set(arr[0], arr[1])
+//       return ''
+//     })
+//     return ''
+//   })
+// });
+
+window.addEventListener('load', function() {
   var container = document.getElementById('content')
-  container.innerHTML = container.innerHTML.replace(/\;;;;denotes([\s\S]*?)\;;;;denotes/gm, (match, content) => {
+  container.innerHTML = container.innerHTML.replace(/\\denotes([\s\S]*?)\\denotes/gm, (match, content) => {
     content.replace(/\;;;;([\s\S]*?);;;;/g, (m, c) => {
       let arr = c.split('::')
       denotes.set(arr[0], arr[1])
@@ -10,9 +22,6 @@ document.addEventListener('DOMContentLoaded', function() {
     })
     return ''
   })
-});
-
-window.addEventListener('load', function() {
   var box = document.createElement('div')
   box.className = 'flex-container'
   var contentContainer = document.getElementById('content')
