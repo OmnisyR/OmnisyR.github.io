becd13cc1778dc38ce005637a06852703ad11c2f
OmnisyR <lxuan233@gmail.com>
2025-08-14 01:51:15 +0000

    🎉auto update by Gmeek action

--- docs/assets/HyperTOC.js
+++ docs/assets/HyperTOC.js
@@ -2,14 +2,14 @@ const denotes = new Map();
 
 document.addEventListener('DOMContentLoaded', function() {
   var container = document.getElementById('content')
-  // container.innerHTML = container.innerHTML.replace(/\\denotes([\s\S]*?)\\denotes/gm, (match, content) => {
-  //   content.replace(/\;;;;([\s\S]*?);;;;/g, (m, c) => {
-  //     let arr = c.split('::')
-  //     denotes.set(arr[0], arr[1])
-  //     return ''
-  //   })
-  //   return ''
-  // })
+  container.innerHTML = container.innerHTML.replace(/\;;;;denotes([\s\S]*?)\;;;;denotes/gm, (match, content) => {
+    content.replace(/\;;;;([\s\S]*?);;;;/g, (m, c) => {
+      let arr = c.split('::')
+      denotes.set(arr[0], arr[1])
+      return ''
+    })
+    return ''
+  })
 });
 
 window.addEventListener('load', function() {
